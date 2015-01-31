5c7f3b9c5acd723684c9725295082d204e660dca
Bill Levien <blevien@safaribooksonline.com>
2015-01-30 21:52:42 -0800

    Add README and usage notes

---
+++ README.md
@@ -0,0 +1,8 @@
+ Read pulse length from Spektrum 2.4 Ghz Reciever AUX Channel for accessory control
+ 3 LEDs are hooked up as outputs to give a visual indication of the position of the AUX
+ signal as read from the Rx.
+
+Eventually, I will build a cool RC Truck Accessory such as a winch, lights, camera gimbal, etc.
+
+Also note that this can easily be powered by the 5v in from the reciever if the accessory does not
+draw alot of current, but I assume it may start to malfunction when the voltage drops.
--- SpektrumAuxChannel.ino
+++ SpektrumAuxChannel.ino
@@ -1,10 +1,17 @@
 /*
  Read pulse length from Spektrum 2.4 Ghz Reciever AUX Channel for accessory control
+ 3 LEDs are hooked up as outputs to give a visual indication of the position of the AUX
+ signal as read from the Rx.
+
+Eventually, I will build a cool RC Truck Accessory such as a winch, lights, camera gimbal, etc.
+
+Also note that this can easily be powered by the 5v in from the reciever if the accessory does not
+draw alot of current, but I assume it may start to malfunction when the voltage drops.
 */
   
 int greenPin = 6;
-int yellowPin = 7;
-int redPin = 8;
+int yellowLight = 7;
+int redLight = 8;
 int receiverPin = 9;
 
 unsigned long duration;
@@ -26,24 +33,24 @@ void loop(){
   if (duration < 1200){
   Serial.println("Setting A");
   digitalWrite(greenPin, HIGH);
-  digitalWrite(yellowPin, LOW);
-  digitalWrite(redPin, LOW);
+  digitalWrite(yellowLight, LOW);
+  digitalWrite(redLight, LOW);
 
   }
   
   if (1400 < duration && duration < 1800){
   Serial.println("Setting B");
   digitalWrite(greenPin, LOW);
-  digitalWrite(yellowPin, HIGH);
-  digitalWrite(redPin, LOW);
+  digitalWrite(yellowLight, HIGH);
+  digitalWrite(redLight, LOW);
 
   }  
   
   if (duration > 1800){
   Serial.println("Setting C");
   digitalWrite(greenPin, LOW);
-  digitalWrite(yellowPin, LOW);
-  digitalWrite(redPin, HIGH);
+  digitalWrite(yellowLight, LOW);
+  digitalWrite(redLight, HIGH);
 
   } 
   
